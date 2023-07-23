f781cb5957540b4bbd7085d0eae311362b5fc311
Rick Herchuk <rickherchuk@gmail.com>
2023-07-23 12:50:19 -0400

    add retrieve current tab

--- popup.js
+++ popup.js
@@ -46,12 +46,36 @@
     return password;
   }
 
+
+  var currentURL = "_";
+
+  async function getCurrentTabUrl () {
+    const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
+    return tabs[0].url;
+  }
+  function displayUrl(current_tab){
+    currentURL= getHostName(current_tab);
+  }
+  function getHostName(url) {
+    var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+    if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+      var hostname = match[2].split(".");
+      return hostname[hostname.length -2];
+    }
+    else {
+      return null;
+    }
+  }
+
 // Pass HTML Variable INTO JS FUNCTION...
+
 function get_plaintext(){
+  getCurrentTabUrl().then(function(value){displayUrl(value)});
   var plaintext = document.getElementById("plaintext").value;
-  document.getElementById('output').innerHTML = encrypt(extend(plaintext), "", "google.com");
-  document.getElementById('check').innerHTML = "Password Generated!"
+  document.getElementById('output').innerHTML = encrypt(extend(plaintext), "", currentURL);
+  document.getElementById('check').innerHTML = currentURL;
 }
 
 
 document.getElementById("submit").addEventListener("click", get_plaintext);
+getCurrentTabUrl().then(function(value){displayUrl(value)});
